f840c309c8303e0e93c7e17eaaa0a4dcc548c792
Adina Rubinoff <adinarubinoff@gmail.com>
2016-12-19 21:05:36 -0500

    doing parts 1 and 2 mathematically

--- 2016/day19.js
+++ 2016/day19.js
@@ -1,30 +1,71 @@
 var number = 3017957;
-var presents = [];
 
-for(var i = 0; i < number; i++) {
-  presents.push(1);
-}
-
-var currElf = 0;
-var currElves = number;
-
-findNextElf = function() {
-  var nextElf = (currElf + 1) % number;
-  while(!presents[nextElf]) {
-    nextElf = (nextElf + 1) % number;
+var run = function(number,part1) {
+  var total = number;
+  var root = {index:1};
+  var curr = root;
+  for(var i = 1; i < number; i++) {
+    curr.next = {index: i+1};
+    curr = curr.next;
   }
-  return nextElf;
-}
+  curr.next = root; //loop it around
 
-while(currElves > 1) {
-  var nextElf = findNextElf();
-  presents[currElf] += presents[nextElf];
-  presents[nextElf] = 0;
-  currElves--; //since we've deleted an elf
-  if(currElves === 1) {
-    //add one to account for index-1 numbering
-    console.log('Elf ' + (currElf + 1) + ' remains ');
+  var currElf = root;
+  while(currElf.next.index !== currElf.index) {
+    if(part1) {
+      //just delete my neighbor
+      currElf.next = currElf.next.next;
+    } else {
+      //subtract one because we're actually killing the "dead elf's" NEXT neighbor
+      var half = Math.floor(total/2) - 1;
+      var deadElf = currElf;
+      while(half) {
+        deadElf = deadElf.next;
+        half--;
+      }
+      deadElf.next = deadElf.next.next; //delete the elf across
+      total--; //keep track of the kills
+      if((total % 1000) === 0) { console.log(total);}
+    }
+    currElf = currElf.next;
+  }
+  if(part1) {
+    console.log('After removing next neighbors, elf ' + currElf.index + ' remains ');
   } else {
-    currElf = findNextElf();
+    console.log(number + '-' + currElf.index);
   }
 }
+
+//this is the slow way of doing it, but it does work
+run(number,true);
+
+//mathematical way based on https://www.youtube.com/watch?v=uCsD3ZGzMgE
+var powerOf2 = Math.pow(2,Math.floor(Math.log2(number)));
+var diff = number - powerOf2;
+console.log('Doing it mathematically: it\'s still elf ' + (diff*2 + 1));
+
+//generate a table for some part2 data
+// for(var i = 3; i < 100; i++) {
+//   run(i,false);
+// }
+
+//conclusion for part 2: it starts over at 1 when it would exceed the number of people
+//if the current run starts at 1 with n people, it goes up by 1 every time, until it
+//passes n, then it starts incrementing by 2
+
+var increment = 1;
+var currStart = 1;
+var winner = 1;
+for(var i = 1; i <= number; i++) {
+  if(increment === 1 && winner >= currStart - 1) {
+    increment = 2;
+  }
+  winner += increment;
+  if (winner > i) {
+    currStart = i;
+    winner = 1;
+    increment = 1;
+  }
+}
+
+console.log('Doing it mathematically, and killing across, the winner with ' + number + ' elves is elf ' + winner);
