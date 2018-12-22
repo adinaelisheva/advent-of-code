2b0c8f8ce10dc93578e2edc3c99d8e47c627d872
Adina Rubinoff <adinarubinoff@gmail.com>
2018-12-22 17:24:40 -0500

    day16 done

--- 2018/day16.py
+++ 2018/day16.py
@@ -11,12 +11,15 @@ class sample:
     return f"{self.before} --> {self.op}({self.arg1}, {self.arg2}) -> {self.out} --> {self.after}"
 
 samples = []
+program = []
 
+# Parse the file:
 with open("day16data") as f:
   lines = f.readlines()
   s = None
   inSample = False
   for l in lines:
+    l = l.strip()
     if l.startswith("Before:"):
       s = sample()
       s.before = [int(x) for x in l[9:19].split(", ")]
@@ -26,14 +29,17 @@ with open("day16data") as f:
       samples.append(s)
       inSample = False
     elif inSample and len(l) > 0:
-      args = l.strip().split(" ")
+      args = l.split(" ")
       s.op = int(args[0])
       s.arg1 = int(args[1])
       s.arg2 = int(args[2])
       s.out = int(args[3])
+    elif len(l) > 0:
+      program.append([int(x) for x in l.split(" ")])
 
 opcodes = {}
 
+# helper to figure out what opcodes a # could be
 def reducePossibilities(olds, news):
   if olds == None:
     return news
@@ -43,6 +49,7 @@ def reducePossibilities(olds, news):
       ret.append(x)
   return ret
 
+# run through all of the samples to see which opcode they could be
 threeOrMore = 0
 for s in samples:
   possibilities = []
@@ -86,12 +93,14 @@ for s in samples:
     opcodes[s.op] = None
   opcodes[s.op] = reducePossibilities(opcodes[s.op], possibilities)
 
+# helper to check if all opcodes are determined
 def isDone(opcodes):
   for o in opcodes:
     if len(opcodes[o]) > 1:
       return False
   return True
 
+# elimination until all opcodes are determined
 while not isDone(opcodes):
   for o in opcodes:
     if len(opcodes[o]) == 1:
@@ -106,3 +115,44 @@ print(f"{threeOrMore} samples could be three or more opcodes")
 for o in opcodes:
   print(f"{o}: {opcodes[o]}")
 
+# run the program
+registers = [0,0,0,0]
+for step in program:
+  op = opcodes[step[0]][0]
+  arg1 = step[1]
+  arg2 = step[2]
+  out = step[3]
+  if op == "addr":
+    registers[out] = registers[arg1] + registers[arg2]
+  if op == "addi":
+    registers[out] = registers[arg1] + arg2
+  if op == "mulr":
+    registers[out] = registers[arg1] * registers[arg2]
+  if op == "muli":
+    registers[out] = registers[arg1] * arg2
+  if op == "banr":
+    registers[out] = registers[arg1] & registers[arg2]
+  if op == "bani":
+    registers[out] = registers[arg1] & arg2
+  if op == "borr":
+    registers[out] = registers[arg1] | registers[arg2]
+  if op == "bori":
+    registers[out] = registers[arg1] | arg2
+  if op == "setr":
+    registers[out] = registers[arg1]
+  if op == "seti":
+    registers[out] = arg1
+  if op == "gtir":
+    registers[out] = 1 if arg1 > registers[arg2] else 0
+  if op == "gtri":
+    registers[out] = 1 if registers[arg1] > arg2 else 0
+  if op == "gtrr":
+    registers[out] = 1 if registers[arg1] > registers[arg2] else 0
+  if op == "eqir":
+    registers[out] = 1 if arg1 == registers[arg2] else 0
+  if op == "eqri":
+    registers[out] = 1 if registers[arg1] == arg2 else 0
+  if op == "eqrr":
+    registers[out] = 1 if registers[arg1] == registers[arg2] else 0
+
+print(registers)
