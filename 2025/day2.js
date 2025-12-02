ef11ff47a8a3a1b724095ee67eac1dde35a68cac
Adina <3931713+adinaelisheva@users.noreply.github.com>
2025-12-02 18:02:43 -0500

    Adding day 2 part 2

--- 2025/day2.js
+++ 2025/day2.js
@@ -2,22 +2,47 @@ const testInput = ['11-22','95-115','998-1012','1188511880-1188511890','222220-2
 const input = ['516015-668918','222165343-222281089','711089-830332','513438518-513569318','4-14','4375067701-4375204460','1490-3407','19488-40334','29275041-29310580','4082818-4162127','12919832-13067769','296-660','6595561-6725149','47-126','5426-11501','136030-293489','170-291','100883-121518','333790-431800','897713-983844','22-41','42727-76056','439729-495565','43918886-44100815','725-1388','9898963615-9899009366','91866251-91958073','36242515-36310763'];
 const INPUT_TO_USE = input;
 
-let sum = 0;
+let sumPt1 = 0;
+let sumPt2 = 0;
 for (const range of INPUT_TO_USE) {
     const start = Number(range.split('-')[0]);
     const end = Number(range.split('-')[1]);
     for (let id = start; id <= end; id++) {
         const numStr = String(id);
-        if (numStr.length % 2 !== 0) {
-            continue;
+        // First check part 1
+        if (numStr.length % 2 === 0) {
+            const splitPt = numStr.length / 2;
+            if (numStr.substring(0, splitPt) === numStr.substring(splitPt)) {
+                console.log(`${id} splits to 2 ${numStr.substring(splitPt)}s, adding ${id} for parts 1 and 2`);
+                sumPt1 += id;
+                sumPt2 += id;
+                continue;
+            }
         }
-        const splitPt = (numStr.length / 2);
-        const num1 = Number(numStr.substring(0, splitPt));
-        const num2 = Number(numStr.substring(splitPt));
-        if (num1 === num2) {
-            sum += id;
-            console.log(`${id} splits to two ${num1}s, adding ${id}`);
+        for (let i = 1; i <= numStr.length / 2; i++) {
+            if (numStr.length % i !== 0) {
+                continue;
+            }
+            const nums = [];
+            let numStrToConsume = `${numStr}`;
+            while (numStrToConsume.length > 0) {
+                nums.push(numStrToConsume.substring(0,i));
+                numStrToConsume = numStrToConsume.substring(i);
+            }
+            let allMatch = true;
+            for (const num of nums) {
+                if (num !== nums[0]) {
+                    allMatch = false;
+                    break;
+                }
+            }
+            if (allMatch) {
+                sumPt2 += id;
+                console.log(`${id} splits to ${i} ${nums[0]}s, adding ${id} for part 2 only`);
+                break;
+            }
         }
     }
 }
-console.log(sum);
+console.log(`Part 1: ${sumPt1}, part 2: ${sumPt2}`);
+
